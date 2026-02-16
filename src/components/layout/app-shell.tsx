94012571b36b961378a3cb25fe96af60eb1d8c57
Rascal <tisankanvc@gmail.com>
2026-02-16 10:20:21 +0530

    Rename Header/Footer to temp names to fix git casing

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { Header } from '@/components/layout/Header';
-import { Footer } from '@/components/layout/Footer';
+import { Header } from '@/components/layout/Header_temp';
+import { Footer } from '@/components/layout/Footer_temp';
 
 export function AppShell({ children }: { children: React.ReactNode }) {
     const pathname = usePathname();
